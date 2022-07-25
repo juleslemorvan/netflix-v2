5aec5f2ea3a7d18d8eaadb85b5317e4c2ba74260
jules <jules.lemorvan01@gmail.com>
2022-07-26 00:07:03 +0200

    login page ok

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,79 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link, useNavigate } from "react-router-dom";
+import { UserAuth } from "../context/AuthContext";
 
 const Login = () => {
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState('')
+
+  const {user, logIn} = UserAuth()
+
+  const navigate = useNavigate()
+
+  const formSubmit = async (e) => {
+    e.preventDefault()
+    setError('')
+    try {
+      await logIn(email, password)
+      navigate("/")
+    }catch (error) {
+      setError(error.message)
+    }
+  }
+
   return (
-    <div>Login</div>
+     <>
+      <div className="w-full h-screen ">
+        <img
+          className="hidden sm:block absolute w-full h-full object-cover "
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/f841d4c7-10e1-40af-bcae-07a3f8dc141a/f6d7434e-d6de-4185-a6d4-c77a2d08737b/US-en-20220502-popsignuptwoweeks-perspective_alpha_website_medium.jpg"
+          alt="/"
+        />
+        <div className="bg-black/60 fixed top-0 left-0 w-full h-screen"></div>
+        <div className="fixed w-full px-4 py-24 z-50">
+          <div className="max-w-[450px] h-[600px] mx-auto bg-black/75 text-white">
+            <div className="max-w-[320px] mx-auto py-16">
+              <h1 className="text-3xl font-bold">Sign Up</h1>
+              <form onSubmit={formSubmit} className="w-full flex flex-col py-4">
+                <input
+                onChange={(e) => setEmail(e.target.value)}
+                  className="p-3 my-2 rounded bg-gray-700"
+                  type="email"
+                  placeholder="email"
+                  autoComplete="email"
+                />
+                <input
+                onChange={(e) => setPassword(e.target.value)}
+                  className="p-3 my-2 rounded bg-gray-700"
+                  type="password"
+                  placeholder="password"
+                  autoComplete="current-password"
+                />
+                {error ? <p className='text-red-600 text-2xl p-5'>{error}</p> : null}
+                <button className="bg-red-600 py-3 my-6 font-bold rounded">
+                  Sign In
+                </button>
+                <div className="flex justify-between items-center text-sm text-gray-600">
+                  <p>
+                    <input className="mr-2" type="checkbox" />
+                    Remember me
+                  </p>
+                  <p>Need Help ?</p>
+                </div>
+                <p className="m-5 text-sm text-gray-600">
+                  Already subscribed to Netflix ?{" "}
+                  <Link className="text-white" to="/login">
+                    Sign-in
+                  </Link>
+                </p>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
